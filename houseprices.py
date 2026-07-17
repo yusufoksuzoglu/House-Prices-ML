02f3730a0723fd18791d6322f07197bd48e6c92f
Yusuf <122007538+yusufoksuzoglu@users.noreply.github.com>
2026-07-17 19:38:23 +0300

    Initialize house price data processing script
    The script processes house price data, identifying and handling missing values, and categorizing features into numerical and categorical columns.

---
+++ houseprices.py
@@ -0,0 +1,73 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# Use the kagglehub client library to attach Kaggle resources like competitions, datasets, and models to your session
+# Learn more about kagglehub: https://github.com/Kaggle/kagglehub/blob/main/README.md
+
+import kagglehub
+
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_error
+from scipy.stats import skew
+#Models
+from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, Lasso
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.ensemble import GradientBoostingRegressor
+from catboost import CatBoostRegressor
+import lightgbm as lgb
+import xgboost as xg
+
+import warnings
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv('../input/competitions/house-prices-advanced-regression-techniques/train.csv')
+test = pd.read_csv("../input/competitions/house-prices-advanced-regression-techniques/test.csv")
+
+data_b = data.drop(["Id"], axis = 1)
+test_b = test.drop(["Id"], axis = 1)
+data_b.head()
+
+data_b.isnull().sum().sort_values(ascending = False).head(20)
+
+#Now we need to determine which columns has NA
+null_in_train = data_b.columns[data_b.isna().any()].tolist()
+null_ony_in_test = list(set(test_b.columns[test_b.isna().any()].tolist())-set(data_b.columns[data_b.isna().any()].tolist()))
+all_null_cols = null_in_train + null_ony_in_test
+
+print("/nNull in Train: " + str(null_in_train))
+print("Null values only in Test: " + str(null_ony_in_test))
+print("/nAll Null Cols: " + str(all_null_cols))
+
+data_b = data_b.drop(all_null_cols, axis = "columns")
+test_b = test_b.drop(all_null_cols, axis = "columns")
+
+y = data_b.pop("SalePrice")
+
+#Now we split columns as categorical and numerical columns
+cols = data_b.columns
+numeric_columns, categorical_columns = [], []
+for i in range(len(cols)):
+    if data_b[cols[i]].dtypes == 'O':
+        categorical_columns.append(cols[i])
+    else:
+        numeric_columns.append(cols[i])
+        
+#To show categorical columns and its unique items
+category_analysis = pd.DataFrame(categorical_columns, columns = ["Feature"])
+unique_values = []
+unique_counts = []
+for col in categorical_columns:
+    unique_values.append(data_b[col].unique())
+    unique_counts.append(len(data_b[col].unique()))
+
+category_analysis["Categories"] = unique_values
+category_analysis["Number"] = unique_counts
+category_analysis
